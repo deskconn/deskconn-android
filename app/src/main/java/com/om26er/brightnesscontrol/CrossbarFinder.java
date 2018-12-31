a2440a511d2dc36c1d2c7d314c97ae11378c98d2
Omer Akram <om26er@gmail.com>
2018-12-31 13:51:36 +0500

    Initial commit

---
+++ app/src/main/java/com/om26er/brightnesscontrol/CrossbarFinder.java
@@ -0,0 +1,80 @@
+package com.om26er.brightnesscontrol;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.nsd.NsdManager;
+import android.net.nsd.NsdServiceInfo;
+import android.util.Log;
+
+public class CrossbarFinder {
+
+    private static final String TAG = CrossbarFinder.class.getName();
+
+    private NsdManager mNsdManager;
+    private NsdManager.DiscoveryListener mDiscoveryListener;
+    private Context mContext;
+
+    public CrossbarFinder(Context context) {
+        mContext = context;
+        mNsdManager = context.getSystemService(NsdManager.class);
+        initialize();
+    }
+
+    public void start() {
+        mNsdManager.discoverServices("_crossbar._tcp", NsdManager.PROTOCOL_DNS_SD,
+                mDiscoveryListener);
+    }
+
+    public void stop() {
+        mNsdManager.stopServiceDiscovery(mDiscoveryListener);
+    }
+
+    private void initialize() {
+        mDiscoveryListener = new NsdManager.DiscoveryListener() {
+            @Override
+            public void onStartDiscoveryFailed(String serviceType, int errorCode) {
+
+            }
+
+            @Override
+            public void onStopDiscoveryFailed(String serviceType, int errorCode) {
+
+            }
+
+            @Override
+            public void onDiscoveryStarted(String serviceType) {
+                Log.d(TAG, "Service discovery started");
+            }
+
+            @Override
+            public void onDiscoveryStopped(String serviceType) {
+
+            }
+
+            @Override
+            public void onServiceFound(NsdServiceInfo serviceInfo) {
+                mNsdManager.resolveService(serviceInfo, new NsdManager.ResolveListener() {
+                    @Override
+                    public void onResolveFailed(NsdServiceInfo serviceInfo, int errorCode) {
+
+                    }
+
+                    @Override
+                    public void onServiceResolved(NsdServiceInfo info) {
+                        Intent intent = new Intent("io.crossbar.found");
+                        intent.putExtra("host", info.getHost().getHostName());
+                        intent.putExtra("port", info.getPort());
+                        intent.putExtra("name", info.getServiceName());
+                        mContext.sendBroadcast(intent);
+                    }
+                });
+            }
+
+            @Override
+            public void onServiceLost(NsdServiceInfo info) {
+                Intent intent = new Intent("io.crossbar.lost");
+                mContext.sendBroadcast(intent);
+            }
+        };
+    }
+}
